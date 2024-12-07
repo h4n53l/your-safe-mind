6d61d5914277a0cb695129c3b10f05f0c9809a81
Jonathan Hansel Awo <jonhan17@gmail.com>
2024-12-07 18:30:04 +0000

    fixed build errors.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { cormorant, crimsonText, lora } from "./fonts/font";
+import { cormorant, crimsonText, lora } from "../fonts/font";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
