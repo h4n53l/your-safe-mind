cc95d9e8c9eb6beade5cb5110ebbc2f0c8926901
Jonathan Hansel Awo <jonhan17@gmail.com>
2024-12-19 19:59:29 +0000

    Added Customer services component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import { cormorant, crimsonText, lora } from "../fonts/font";
 import PrelineScript from "@/components/PrelineScript";
 import AnimatedGradientBackground from "@/components/layout/AnimatedGradientBackground";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -22,6 +23,17 @@ export default function App({ Component, pageProps }: AppProps) {
     </Layout>
     <PrelineScript />
     </main>
+    <Script  type="text/javascript" id="tawk" strategy="lazyOnload">
+{`var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/676478d4af5bfec1dbde74e3/1ifg7fvpr';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`}
+    </Script>
     </>
 );
 }
